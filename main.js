1ce0b3fa39b6b3da9b5e8341bf1e5dc38da7b3e7
Samuel Uzelino <uzelino.sam@gmail.com>
2025-03-23 17:26:09 +0100

    Update map coordinates and marker popup in main.js to reflect Istanbul location

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-ap = L.map('map').setView([-39.48, 176.90], 13);
+var map = L.map('map').setView([41.01, 28.96], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-39.48, 176.90]).addTo(map);
-marker.bindPopup("<b>Napier</b><br>Center").openPopup();
+var marker = L.marker([41.01, 28.96]).addTo(map);
+marker.bindPopup("<b>Istanbul</b><br>City Center").openPopup();
