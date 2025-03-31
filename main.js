a14e0f8f31580dd164decbf3f73fb25ae527cf1c
Samuel Uzelino <uzelino.sam@gmail.com>
2025-03-31 10:32:01 +0200

    Refactor map initialization in main.js to use separate variables for latitude, longitude, and zoom level

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let map = L.map('map').setView([41.01, 28.96], 13);
+let lat = 41.01;
+let lng = 28.96;
+let zoom = 13;
+
+// Initialize the map and set its view to the specified location and zoom level
+let map = L.map('map').setView([lat, lng], zoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
