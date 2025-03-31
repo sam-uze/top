d91bb87bcc558776ec71a3ec6807dbac28a76e53
Samuel Uzelino <uzelino.sam@gmail.com>
2025-03-31 10:44:12 +0200

    Refactor map initialization in main.js to use a stop object for location details

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-let lat = 41.01;
-let lng = 28.96;
-let zoom = 13;
+
+let stop = {
+    nr: 7,
+    title: "Istanbul",
+    user: "sam-uze",
+    lat: 41.01,
+    lng: 28.96,
+    zoom: 13,
+};
 
 // Initialize the map and set its view to the specified location and zoom level
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([41.01, 28.96]).addTo(map);
+let marker = L.marker([stop.lat, stop.lng]).addTo(map);
 marker.bindPopup(`
-    <h2>Istanbul</h2>
+    <h2>${stop.title}</h2>
     <ul>
-        <li>geogr. Breite: ${lat.toFixed(5)}</li>
-        <li>geogr. Länge: ${lng.toFixed(5)}</li>
+        <li>geogr. Breite: ${stop.lat.toFixed(5)}°</li>
+        <li>geogr. Länge: ${stop.lng.toFixed(5)}°</li>
     </ul>
 `).openPopup();
