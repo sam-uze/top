0708973219960f7ae1ff02d25e67fd69dc853d9e
Samuel Uzelino <uzelino.sam@gmail.com>
2025-03-31 10:29:12 +0200

    Refactor variable declarations in main.js to use 'let' for map and marker

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([41.01, 28.96], 13);
+let map = L.map('map').setView([41.01, 28.96], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([41.01, 28.96]).addTo(map);
+let marker = L.marker([41.01, 28.96]).addTo(map);
 marker.bindPopup("<b>Istanbul</b><br>City Center").openPopup();
