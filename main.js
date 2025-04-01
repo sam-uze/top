4c0a5958a077167475668e936b3871fb5030f29b
Samuel Uzelino <uzelino.sam@gmail.com>
2025-04-01 11:33:43 +0200

    Add onchange event for pulldown menu to navigate to selected URL

--- main.js
+++ main.js
@@ -232,3 +232,12 @@ for ( let i=0; i<STOPS.length; i++){
     }
     document.querySelector("#pulldown select").appendChild(option);
 }
+
+//auf Änderungen beim Pulldown reagieren
+document.querySelector("#pulldown select").onchange = function(evt){
+    let url = `https://${evt.target.value}.github.io/nz`;
+    //console.log(url);
+    //console.log(evt.target.value);
+    window.location = url;
+    
+}
